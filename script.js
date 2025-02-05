3e577f917971294f093aa506c2bf9b6bb840aa08
Ankit jain <83234209+jain009@users.noreply.github.com>
2025-02-06 02:10:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,7 +9,15 @@ function convertToRoman(num) {
       6:['I', 1]
     };
 
-  //your code here
+ let result = '';
+    for (const [value, symbol] of romanNumerals) {
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
+    }
+    return result;
+}
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
